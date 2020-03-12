969802c73f40530ed6fae7e4557efdd8288a5249
themanyone <nospam@thenerdshow.com>
2020-03-11 21:49:39 -0800

    first commit

---
+++ draggable.js
@@ -0,0 +1,86 @@
+
+  /* Draggable Elements: Drag and dragElements elements with mouse or gestures */
+  //Adapted to optionally work with titlebars.
+  //References: 
+     //www.w3schools.com/howto/tryit.asp?filename=tryhow_js_draggable
+     //www.kirupa.com/html5/drag.htm
+     //www.kirupa.com/html5/examples/drag_multiple.htm
+  function dragElements(e){
+    var container = document.querySelector(e);
+    var activeItem = null, ca = container.addEventListener;
+    
+    ca("touchstart", dragStart, false);
+    ca("touchend", dragEnd, false);
+    ca("touchmove", drag, false);
+
+    ca("mousedown", dragStart, false);
+    ca("mouseup", dragEnd, false);
+    ca("mousemove", drag, false);
+    
+    //create some events
+    var startmove = new CustomEvent('startmove');
+    var moving = new CustomEvent('moving');
+    var stopmove = new CustomEvent('stopmove');
+
+    function dragStart(e) {
+      var et = e.target;
+      //activeItem is either a parent of a titlebar, or drag
+      if (et.classList.contains("titlebar")){ et = et.parentElement; }
+      else {//find something to drag
+         while (!et.classList.contains("drag")){
+            //if nothing is drag, we're done here
+            if (!et.parentElement || et === container) return;
+            et = et.parentElement; //keep looking
+         }
+      } activeItem = et;
+      
+      //move on top of other elements
+      for (i = container.children.length; i--;)
+         container.children[i].style.zIndex="";
+      activeItem.style.zIndex="1";
+
+       if (!activeItem.xOffset) { activeItem.xOffset = 0; }
+       if (!activeItem.yOffset) { activeItem.yOffset = 0; }
+      
+       if (e.type === "touchstart") {
+         activeItem.initialX = e.touches[0].clientX - activeItem.xOffset;
+         activeItem.initialY = e.touches[0].clientY - activeItem.yOffset;
+       } else {
+         //console.log("Dragging", e.etet.tagName);
+         activeItem.initialX = e.clientX - activeItem.xOffset;
+         activeItem.initialY = e.clientY - activeItem.yOffset;
+       }
+        activeItem.dispatchEvent(startmove);
+    }
+
+    function dragEnd(e) {
+      if (activeItem) {
+        activeItem.initialX = activeItem.currentX;
+        activeItem.initialY = activeItem.currentY;
+        activeItem.dispatchEvent(stopmove);
+      }
+      activeItem = null;
+    }
+
+    function drag(e) {
+      if (activeItem) {
+        if (e.type === "touchmove") {
+          e.preventDefault();
+          activeItem.currentX = e.touches[0].clientX - activeItem.initialX;
+          activeItem.currentY = e.touches[0].clientY - activeItem.initialY;
+        } else {
+          activeItem.currentX = e.clientX - activeItem.initialX;
+          activeItem.currentY = e.clientY - activeItem.initialY;
+        }
+        activeItem.xOffset = activeItem.currentX;
+        activeItem.yOffset = activeItem.currentY;
+        setTranslate(activeItem.currentX, activeItem.currentY, activeItem);
+        activeItem.dispatchEvent(moving);
+      }
+    }
+
+    function setTranslate(xPos, yPos, el) {
+      el.style.transform = "translate(" + xPos + "px, " + yPos + "px)";
+    }
+  }
+  window.addEventListener('load', dragElements(".draggable"));
