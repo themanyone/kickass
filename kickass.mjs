3a4864ce14e533fb9bb464fd9f6fce3f5f258f40
themanyone <nospam@thenerdshow.com>
2020-03-28 23:11:53 -0800

    update

--- kickass.mjs
+++ kickass.mjs
@@ -1,10 +1,6 @@
 // Kickass Copyright (C) 2014-2020 by Henry Kroll, www.thenerdshow.com
-_doc = document, _body = _doc.body, _get = e=> _doc.getElementById(e);
-_qs  = e=> _doc.querySelector(e); _qsa = e=> _doc.querySelectorAll(e);
-_echo = qs=>_doc.write(_qs(qs).innerHTML);
-
-Object.prototype._qs = function(q){return this.querySelector(q);};
-Object.prototype._qsa = function(q){return this.querySelectorAll(q);};
+var _doc  = document, _body = _doc.body, _get = e=> _doc.getElementById(e),
+    _qs = qs=>_doc.querySelector(qs);
 
 var _dce = function(tagName, id=null, classNames=null){
   var ret =_doc.createElement(tagName);
@@ -13,18 +9,14 @@ var _dce = function(tagName, id=null, classNames=null){
 };
 
 var htmlEntities = function(str) {
-  return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-};
-
-var echoCode = function(qs, type){
-  var t = htmlEntities(_qs(qs).outerHTML);
-  _doc.write("<pre><code class="+type+">  ", t, "\n</code></pre>");
+  return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;')
+  .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 };
 
 var forAll = function(obj, cb) {
   return (typeof cb === "function")
   ? obj.forAll(cb) : typeof obj === "object"
-    ? function(e) { obj.forAll(e); } : undefined;
+    ? e=>obj.forAll(e) : undefined;
 };
 
 Object.prototype.forAll = function(cb) {
@@ -39,3 +31,13 @@ Array.prototype.uniq = function(){
 	return Object.keys(obj);
 };
 
+(e=>{
+  //process replacement commands
+  var h = _body.innerHTML
+    .replace(/_echo\(([^)]+)\)/gi, (match,p1)=>_qs(p1).innerHTML)
+    .replace(/_code\(([^)]+)\)/gi, (match,p1)=>
+      "<pre><code>" + htmlEntities(_qs(p1).outerHTML) + "</code></pre>");
+  _body.innerHTML = h;
+})();
+
+export {  };
